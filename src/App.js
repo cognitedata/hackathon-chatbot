b257e63cb5a5c8e2d793355464ecf45563ac753e
David Liu <david.liu@cognite.com>
2019-10-02 12:09:31 +0200

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ChatBot from 'react-simple-chatbot';
+
+const steps = [
+	{
+		id: '0',
+		message: 'Welcome to Cognite chatbot!',
+		trigger: '1',
+	},
+	{
+		id: '1',
+		user: true,
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ChatBot recognitionEnable={true} steps={steps} />
+		</div>
+	);
 }
 
 export default App;
